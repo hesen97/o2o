490c5c49ae047f30ad73346fff90f1eaab569a66
hesen97 <sen_he@yeah.net>
2020-11-25 15:45:49 +0800

    测试Controller层

---
+++ src/main/java/com/hesen/o2o/web/superadmin/AreaController.java
@@ -0,0 +1,43 @@
+package com.hesen.o2o.web.superadmin;
+
+import com.hesen.o2o.entity.Area;
+import com.hesen.o2o.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/superadmin")
+@Controller
+public class AreaController {
+
+    @Autowired
+    private AreaService areaService;
+
+    @ResponseBody
+    @RequestMapping(value = "/areas", method = RequestMethod.GET)
+    public Map<String, Object> getAreaList() {
+        //这里是不是可以使用Spring自带的ModelMap，或者Modal，为啥要自己写？？
+        Map<String, Object> modelMap = new HashMap<>();
+        List<Area> areaList = null;
+
+        try {
+            //为啥要放到try catch中，根本就没啥异常啊？？
+            areaList = areaService.getAreaList();
+            modelMap.put("success", true);
+            modelMap.put("rows", areaList);
+            modelMap.put("total", areaList.size());
+        } catch (Exception e) {
+            modelMap.put("success", false);
+            modelMap.put("errorMsg", e.getMessage());
+            e.printStackTrace();
+        }
+        return modelMap;
+    }
+
+}
